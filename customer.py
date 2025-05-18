0781578713c00019e049fdd7b5233e1dcdb48221
kiago <roy.kiago@student.moringaschool.com>
2025-05-18 13:12:03 +0300

    Customers

--- customer.py
+++ customer.py
@@ -15,3 +15,20 @@ class Customer:
             self._name = value
         else:
             raise ValueError("Name must be a string between 1 and 15 characters.")
+
+    def orders(self):
+        return [order for order in Order.all_orders if order.customer == self]
+
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        customer_spending = {}
+        for order in Order.all_orders:
+            if order.coffee == coffee:
+                customer_spending[order.customer] = customer_spending.get(order.customer, 0) + order.price
+        return max(customer_spending, key=customer_spending.get, default=None)
