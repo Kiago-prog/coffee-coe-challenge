63e1dbc264b7df85967cc77324005d218e49558f
kiago <roy.kiago@student.moringaschool.com>
2025-05-18 13:14:23 +0300

    Coffee

--- coffee.py
+++ coffee.py
@@ -8,9 +8,22 @@ class Coffee:
         else:
             raise ValueError("Coffee name must be a string with at least 3 characters.")
 
+
     @property
     def name(self):
         return self._name  # Immutable
 
     def orders(self):
         return [order for order in Order.all_orders if order.coffee == self]
+
+    def customers(self):
+        return list({order.customer for order in self.orders()})
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        return sum(order.price for order in orders) / len(orders)
