083b9a94047610dd6701ca9f342d069ab4f40669
kiago <roy.kiago@student.moringaschool.com>
2025-05-18 13:29:56 +0300

    Customer Test

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -11,3 +11,14 @@ class TestCustomer(unittest.TestCase):
     def test_customer_init_valid_name(self):
         customer = Customer("Alice")
         self.assertEqual(customer.name, "Alice")
+
+    def test_customer_invalid_name_type(self):
+        with self.assertRaises(ValueError):
+            Customer(123)
+
+    def test_customer_invalid_name_length(self):
+        with self.assertRaises(ValueError):
+            Customer("")
+
+        with self.assertRaises(ValueError):
+            Customer("x" * 16)
