618b5bcb1f007525ec8931e890ab4d75b4440265
kiago <roy.kiago@student.moringaschool.com>
2025-05-18 11:57:51 +0300

    Customer

--- customer.py
+++ customer.py
@@ -4,3 +4,14 @@ class Customer:
     def __init__(self, name):
         self.name = name
         Customer.all_customers.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")
